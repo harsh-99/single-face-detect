085709b4f475b36c96e97f6260ac22aa5e5eced3
harsh <harsh-99>
2017-12-19 17:30:55 +0530

    with same loss function

--- singlehuman_detect.py
+++ singlehuman_detect.py
@@ -1,7 +1,3 @@
-from __future__ import absloute_import 
-from __future__ import division
-from __future__ import print_function
-
 import argparse 
 import sys
 
@@ -47,6 +43,7 @@ for i in range(0,num_images):
 		img = cv2.resize(img,(100,100))
 		im.append(img)
 		y_label.append(0)
+		y_linear.append(-1,-1,-1,-1)
 	else : 
 		name  = ''	#depends on the folder
 		img = cv2.imread(name)
@@ -55,14 +52,13 @@ for i in range(0,num_images):
 		im.append(img)
 		y_label.append(1)   #change the value of factor(2) if the factor is different 
 		y_linear.append(a)
-		im_linear.append(img)
 
 X_in = np.array(im)
 Y_in = np.array(y_label)
 Y_in_linear = np.array(y_linear)
-im_in_linear = np.array(im_linear)
+# im_in_linear = np.array(im_linear)
 print X_in.shape
-im_in_linear = np.reshape(im_in_linear, (num_pos_images, X_in.shape[1]*X_in.shape[2]*X_in.shape[3]))
+# im_in_linear = np.reshape(im_in_linear, (num_pos_images, X_in.shape[1]*X_in.shape[2]*X_in.shape[3]))
 X_in = np.reshape(X_in, (num,images, X_in.shape[1]*X_in.shape[2]*X_in.shape[3]))
 Y_in = np.reshape(Y_in, (num_images, 1))
 Y_in_linear = np.reshape(Y_in_linear,(num_pos_images, 4))
@@ -75,9 +71,9 @@ W_conv1 = weight_variable([5,5,3,32])
 b_conv1 = bias_variable([32])
 
 X = tf.placeholder(tf.float32,[NONE, X_in.shape[1]])
-Y = tf.placeholder(tf.float32. [NONE, 1])
+Y_label = tf.placeholder(tf.float32. [NONE, 1])
 
-X_linear = tf.placeholder(tf.float32, [NONE, im_in_linear.shape[1]])
+# X_linear = tf.placeholder(tf.float32, [NONE, im_in_linear.shape[1]])
 Y_linear1 = tf.placeholder(tf.float32, [NONE, 4])	
 
 X_image = tf.reshape(X, [-1,100,100,3])
@@ -102,36 +98,42 @@ b_conv4 = bias_variable([256])
 h_conv4 = tf.nn.relu(conv2d(h_poll2, W_conv4)+b_conv4)
 h_poll3 = max_pool_2x2(h_conv4)
 
-W_fc1 = weight_variable([25*25*256, 1024])
-b_fc1 = bias_variable([1024])
+W_fc1_label = weight_variable([25*25*256, 1024])
+b_fc1_label = bias_variable([1024])
+
+W_fc1_linear = weight_variable([25*25*256, 1024])
+b_fc1_linear = bias_variable([1024])
 
 h_pool3_flat = tf.reshape(h_poll3,[-1, 25*25*256])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1)+b_fc1)
+h_fc1_linear = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1_linear)+b_fc1_linear)
+h_fc1_label = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1_label)+b_fc1_label)
 
 W_fc2_linear = weight_variable([1024,4])
 b_fc2_linear = bias_variable([4])
 
-W_fc2 = weight_variable([1024,2])
-b_fc2 = bias_variable([2]) 
+W_fc2_label = weight_variable([1024,1])
+b_fc2_label = bias_variable([1]) 
 
-y_conv = tf.matmul(h_fc1, W_fc2)+b_fc2
-y_conv_linear = tf.matmul(h_fc1, W_fc2_linear)+b_fc2_linear 
+y_conv_label = tf.matmul(h_fc1_label, W_fc2_label)+b_fc2_label
+y_conv_linear = tf.matmul(h_fc1_linear, W_fc2_linear)+b_fc2_linear
 
-cost = tf.reduce_sum(tf.pow(y_conv_linear-Y_in_linear)/(2*num_pos_images)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+loss = tf.reduce_mean(-1*Y_label*(tf.log(y_conv_label)+tf.reduce_sum(tf.pow(y_conv_linear-Y_linear1, 2)),1)-(1-Y_label)*tf.log(1-y_conv_label)) 
+train_step=tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-predicted=Y if ((y_conv>0.5 and Y==1) or (y_conv<0.5 and Y==0)) else (1-Y)
-correct_prediction=tf.equal(predicted,Y)
-accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))*100
+# cost = tf.reduce_sum(tf.pow(y_conv_linear-Y_in_linear)/(2*num_pos_images)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+
+# cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
+# train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+# predicted=Y if ((y_conv>0.5 and Y==1) or (y_conv<0.5 and Y==0)) else (1-Y)
+# correct_prediction=tf.equal(predicted,Y)
+# accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))*100
 with tf.Session() as sess:
 	sess.run(tf.gloabal_variables_initializer())
 	saver = tf.train.saver()
 	#*******Training******
 	for i in range(20000):   # change according to the number of images
-		train_step.run(feed_dict{X:X_in, Y:Y_in})
-		sess.run(optimizer,feed_dict= {X_linear: im_in_linear,Y_linear1: Y_in_linear })
-
+		train_step.run(feed_dict{X:X_in, Y_label:Y_in, Y_linear1: Y_in_linear})
 
